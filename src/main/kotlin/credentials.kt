a576a318b1e01394a697104b3037703d6df84b82
Dr AlienTech <alientechgamig@hotmail.com>
2021-01-24 11:34:00 -0500

    fixed json imports

--- src/main/kotlin/credentials.kt
+++ src/main/kotlin/credentials.kt
@@ -1,3 +1,12 @@
-import org.jetbrains.kotlinx
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.jetbrains.kotlin.*
+
 @Serializable
 data class Data(val a: Int, val b: String)
+
+fun main() {
+    val data = Data(1, "Hi Royce")
+    println(Json.encodeToString(data))
+}
