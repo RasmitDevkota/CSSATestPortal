35626a887b80eec9b663bd9f529801105dc8d977
Royce Arockiasamy <royceaden@gmail.com>
2021-03-02 10:43:36 -0500

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -18,13 +18,13 @@ function auth() {
                 
                 firebase.auth().signInWithEmailAndPassword(valueArray[0], valueArray[4]).then(() => {
                     console.log("Signed in!");
-                    setCookie("username") = valueArray[1];
+                    setCookie('username',valueArray[1],365);
                     window.location.href = "dashboard.html";
                 }).catch((error) => {
                     if (error.code == "auth/wrong-password") {
                         firebase.auth().signInWithEmailAndPassword(valueArray[0], pwd).then(() => {
                             firebase.auth().currentUser.updatePassword(valueArray[4]).then(function() {
-                                setCookie("username") = valueArray[1];
+                                setCookie('username',valueArray[1],365);
                                 console.log("Success");
                             }).catch(function(error) {
                                 console.error(error);
@@ -71,7 +71,7 @@ function signUp() {
                 firebase.auth().createUserWithEmailAndPassword(emailC, hashed).then(function () {
                     firebase.auth().signInWithEmailAndPassword(emailC, hashed).then(() => {
                         console.log("Signed in!");
-                        setCookie("username") = usr;
+                        setCookie('username',usr,365);
                         window.location.href = "dashboard.html";
                     }).catch(function (error) {
                         console.log(error);
@@ -159,9 +159,9 @@ function attachSignin(element) {
 
                             firebase.auth().signInWithEmailAndPassword(valueArray[0], valueArray[4]).then(() => {
                                 console.log("Signed in!");
-                                setCookie("username") = valueArray[1];
+                                setCookie('username',valueArray[1],365);
                                 
-                                //window.location.href = "dashboard.html";
+                                window.location.href = "dashboard.html";
                             }).catch((error) => {
                                 if (error.code == "auth/wrong-password") {
                                     alert("There's a small problem with your account, but don't worry, we can fix it very easily!\n\nContact crewcssa@gmail.com or join our Discord at bit.ly/cssa-discord for assistance!")
@@ -197,7 +197,7 @@ function attachSignin(element) {
                                 firebase.auth().createUserWithEmailAndPassword(profile.getEmail(), hashed).then(function () {
                                     firebase.auth().signInWithEmailAndPassword(profile.getEmail(), hashed).then(() => {
                                         console.log("Signed in!");
-                                        setCookie("username") = username;
+                                        setCookie('username',username,365);
                                         window.location.href = "dashboard.html";
                                     }).catch(function (error) {
                                         console.log(error);
